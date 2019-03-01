474e50424cd4a24dbfe8f8b19a852564df5b701f
mondadel <45176185+mondadel@users.noreply.github.com>
2019-03-01 10:23:39 +0100

    Update headerFooter.js

--- headerFooter.js
+++ headerFooter.js
@@ -1,6 +1,6 @@
 
-var importHeaderJs = false;
-const RSI_BASE_URL = "https://rsi.ch/";
+const RSI_BASE_URL = "https://www.rsi.ch";
+
 
 function getHeader(headerId,sectionId) {
 
@@ -8,11 +8,6 @@ function getHeader(headerId,sectionId) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById(headerId).innerHTML = this.responseText;
-
-            var imported = document.createElement('script');
-            imported.src = RSI_BASE_URL + '/static/newHome/js/main.js';
-            document.head.appendChild(imported);
-            importHeaderJs = true;
         }
     };
     xhttp.open("GET", RSI_BASE_URL + "/rsi-api/headerFooter/header/"+sectionId, true);
@@ -24,19 +19,18 @@ function getFooter(footerId,sectionId) {
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById(footerId).innerHTML = this.responseText;
+            console.log(this.responseText);
 
-            if(!importHeaderJs){
-                var imported = document.createElement('script');
-                imported.src = RSI_BASE_URL + '/static/newHome/js/main.js';
-                document.head.appendChild(imported);
-            }
+            var imported = document.createElement('script');
+            imported.src = RSI_BASE_URL + '/static/newHome/js/main-min.js';
+            document.head.appendChild(imported);
         }
     };
-    xhttp.open("GET",  RSI_BASE_URL + "/rsi-api/headerFooter/footer/"+sectionId, true);
+    xhttp.open("GET", RSI_BASE_URL + "/rsi-api/headerFooter/footer/"+sectionId, true);
     xhttp.send();
 }
 
-function getHeaderFooter(headerId, footerId, sectionId){
+function getHeaderFooter(headerId, footerId,sectionId){
     var xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
@@ -46,11 +40,12 @@ function getHeaderFooter(headerId, footerId, sectionId){
             document.getElementById(footerId).innerHTML = splitStr[1];
 
             var imported = document.createElement('script');
-            imported.src = RSI_BASE_URL + '/static/newHome/js/main.js';
+            imported.src = '/static/newHome/js/main.js';
             document.head.appendChild(imported);
+
         }
     };
-    xhttp.open("GET",  RSI_BASE_URL + "rsi-api/headerFooter/headerfooter/"+sectionId, true);
+    xhttp.open("GET", "/rsi-api/headerFooter/headerfooter/"+sectionId, true);
     xhttp.send();
 }
 
@@ -58,3 +53,8 @@ function isHidden(el) {
     var style = window.getComputedStyle(el);
     return (style.display === 'none')
 }
+
+function radioChannel(mediaURL) {
+    window.open(mediaURL, 'Player', 'resizable=no,width=201,height=430 ,left=' + (screen.width - 214) + ',top=5,screenX=0,screenY=0');
+}
+
